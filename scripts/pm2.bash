f9f29407536c953f0e10eb6090ac5111d57ad44a
Zain <reply2zain@gmail.com>
2023-01-16 18:09:04 -0500

    chore: update pm2 script file

--- scripts/pm2.bash
+++ scripts/pm2.bash
@@ -1,4 +1,3 @@
-
-echo "[NOTICE] Ensure tsc was built"
 echo "[NOTICE] Ensure localData.txt is up to date"
-pm2 start node dist/index.js --name db-bank
+sleep 3
+tsc && pm2 start node dist/index.js --name db-bank
