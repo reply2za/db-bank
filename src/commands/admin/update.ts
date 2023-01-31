b722583486b0bf39937f59fd8830ebed3bfae2c4
Zain <reply2zain@gmail.com>
2023-01-30 22:26:06 -0500

    refactor: add update cmd

---
+++ src/commands/admin/update.ts
@@ -0,0 +1,7 @@
+import { MessageEventLocal } from '../../utils/types';
+import { execSync } from 'child_process';
+
+exports.run = async (event: MessageEventLocal) => {
+    event.message.channel.send('updating...');
+    execSync('git stash && git pull && npm i && pm2 delete db-bank && npm run pm2');
+};
