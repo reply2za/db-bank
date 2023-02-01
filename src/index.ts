2537de96dcdb629f5aadb2958a8c1d79cd5832b5
Zain <reply2zain@gmail.com>
2023-01-31 19:56:18 -0500

    refactor: add formal eventHandler

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,12 @@ import { TextChannel } from 'discord.js';
 import { bot, isDevMode } from './utils/constants';
 import { bank } from './finance/Bank';
 import { localStorage } from './Storage/LocalStorage';
-import fs from 'fs';
 import { commandHandler } from './handlers/CommandHandler';
+import { eventHandler } from './handlers/EventHandler';
 
 require('dotenv').config();
 
 const token = process.env.CLIENT_TOKEN?.replace(/\\n/gm, '\n');
-
-function listenForMessages() {
-    commandHandler.loadAllCommands();
-    const files = fs.readdirSync('./dist/events').filter((file) => file.endsWith('.js'));
-    for (const file of files) {
-        const eventName = file.split('.')[0];
-        const event = require(`./events/${file}`);
-        bot.on(eventName, event);
-    }
-    console.log('-loaded events-');
-}
-
-async function loadData() {
-    const data = await localStorage.retrieveData();
-    if (data) {
-        await bank.deserializeAndLoadData(data, bot.users);
-    } else {
-        console.log('[WARN] No local data!');
-    }
-}
 
 process.on('uncaughtException', (error) => {
     console.log(error);
@@ -42,7 +22,13 @@ process.on('uncaughtException', (error) => {
     await bot.login(token);
     console.log('-logged in-');
     console.log('loading data...');
-    await loadData();
-    console.log('-data loaded-');
-    listenForMessages();
+    const data = await localStorage.retrieveData();
+    if (data) {
+        await bank.deserializeAndLoadData(data, bot.users);
+        console.log('-data loaded-');
+    } else {
+        console.log('[WARN] No local data!');
+    }
+    commandHandler.loadAllCommands();
+    eventHandler.loadAllEvents();
 })();
