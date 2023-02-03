3eada09d8efe4a00fbf97d8c9ee90deeda9200f6
Zain <reply2zain@gmail.com>
2023-02-02 19:55:30 -0500

    refactor: remove update from clientCommands

--- src/commands/admin/update.ts
+++ src/commands/admin/update.ts
@@ -2,6 +2,6 @@ import { MessageEventLocal } from '../../utils/types';
 import { execSync } from 'child_process';
 
 exports.run = async (event: MessageEventLocal) => {
-    event.message.channel.send('updating...');
+    await event.message.channel.send('updating...');
     execSync('git stash && git pull && npm i && pm2 restart db-bank');
 };
