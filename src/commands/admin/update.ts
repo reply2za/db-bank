9eb98469a77856f57f82ffc72391822a430ade0f
Zain <reply2zain@gmail.com>
2023-02-02 21:58:46 -0500

    refactor: build ts project on update

--- src/commands/admin/update.ts
+++ src/commands/admin/update.ts
@@ -3,5 +3,5 @@ import { execSync } from 'child_process';
 
 exports.run = async (event: MessageEventLocal) => {
     await event.message.channel.send('updating...');
-    execSync('git stash && git pull && npm i && pm2 restart db-bank');
+    execSync('git stash && git pull && npm i && tsc && pm2 restart db-bank');
 };
