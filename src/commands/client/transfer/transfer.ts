fd2aa61579061bccb58de32a4c0abccdb5f0a6e4
Zain <reply2zain@gmail.com>
2023-02-25 19:29:30 -0500

    refactor: remove TransferManager class

--- src/commands/client/transfer/transfer.ts
+++ src/commands/client/transfer/transfer.ts
@@ -1,7 +1,12 @@
-import { getUserToTransferTo } from '../../../utils/utils';
-import { TransferManager } from '../../../finance/TransferManager';
+import { getUserResponse, getUserToTransferTo, roundNumberTwoDecimals } from '../../../utils/utils';
 import { commandHandler } from '../../../handlers/CommandHandler';
 import { MessageEventLocal } from '../../../utils/types';
+import { TextBasedChannel } from 'discord.js';
+import { BankUser } from '../../../finance/BankUser';
+import { BankVisualizer } from '../../../finance/BankVisualizer';
+import { getAmount, validateMonetaryAmount } from '../../../utils/numberUtils';
+import { bank } from '../../../finance/Bank';
+import { TransferType } from '../../../finance/types';
 
 exports.run = async (event: MessageEventLocal) => {
     if (event.args[1]?.toLowerCase() === 'iou') {
@@ -10,6 +15,38 @@ exports.run = async (event: MessageEventLocal) => {
     } else {
         const recipientBankUser = await getUserToTransferTo(event.message, event.args[1]);
         if (!recipientBankUser) return;
-        await new TransferManager(event.bankUser).processMonetaryTransfer(event.message.channel, recipientBankUser);
+        await processMonetaryTransfer(event.message.channel, event.bankUser, recipientBankUser);
     }
 };
+
+/**
+ * Performs the process of a monetary transfer.
+ * @param channel The text channel to use.
+ * @param sender The BankUser sending the money.
+ * @param receiver The BankUser receiving the money.
+ */
+async function processMonetaryTransfer(channel: TextBasedChannel, sender: BankUser, receiver: BankUser) {
+    let transferEmbed = BankVisualizer.getCashTransferEmbed(sender, receiver, 0);
+    const embedMsg = await transferEmbed.send(channel);
+    const responseAmt = await getAmount(channel, sender.userId);
+    if (responseAmt === 'q') {
+        channel.send('*cancelled transfer*');
+        embedMsg.deletable && embedMsg.delete();
+        return;
+    }
+    const transferAmount = roundNumberTwoDecimals(Number(responseAmt));
+    const isValid = validateMonetaryAmount(transferAmount, sender, channel);
+    if (!isValid) {
+        embedMsg.deletable && embedMsg.delete();
+        return;
+    }
+    transferEmbed = BankVisualizer.getCashTransferEmbed(sender, receiver, transferAmount);
+    await transferEmbed.edit(embedMsg);
+    await BankVisualizer.getPreTransferConfirmationEmbed().send(channel);
+    const responseConfirmation = (await getUserResponse(channel, sender.userId))?.content;
+    if (responseConfirmation && responseConfirmation.toLowerCase() === 'yes') {
+        await bank.transferAmount(sender, receiver, transferAmount, channel, TransferType.TRANSFER);
+    } else {
+        await channel.send('*cancelled transfer*');
+    }
+}
--- src/commands/client/transferiou.ts
+++ src/commands/client/transferiou.ts
@@ -1,9 +1,77 @@
-import { TransferManager } from '../../finance/TransferManager';
-import { getUserToTransferTo } from '../../utils/utils';
+import { getUserResponse, getUserToTransferTo, roundNumberTwoDecimals } from '../../utils/utils';
 import { MessageEventLocal } from '../../utils/types';
+import { GuildTextBasedChannel, If, TextBasedChannel } from 'discord.js';
+import { BankUser } from '../../finance/BankUser';
+import { BankVisualizer } from '../../finance/BankVisualizer';
+import { getAmount, validateAmount } from '../../utils/numberUtils';
+import { bank } from '../../finance/Bank';
+import { localStorage } from '../../storage/LocalStorage';
+import Logger from '../../utils/Logger';
 
 exports.run = async (event: MessageEventLocal) => {
     const recipientBankUser = await getUserToTransferTo(event.message, event.args[1]);
     if (!recipientBankUser) return;
-    await new TransferManager(event.bankUser).processIOUTransfer(event.message.channel, recipientBankUser);
+    await processIOUTransfer(event.message.channel, event.bankUser, recipientBankUser);
 };
+
+/**
+ * Performs the process of an IOU transfer.
+ * @param channel The text channel to use.
+ * @param sender The BankUser sending the IOU
+ * @param receiver The BankUser receiving the IOU
+ */
+async function processIOUTransfer(
+    channel: If<boolean, GuildTextBasedChannel, TextBasedChannel>,
+    sender: BankUser,
+    receiver: BankUser
+) {
+    let transferEmbed = BankVisualizer.getIOUTransferEmbed(sender, receiver, 0);
+    const embedMsg = await transferEmbed.send(channel);
+    const responseAmt = await getAmount(channel, sender.userId);
+    if (responseAmt === 'q') {
+        channel.send('*cancelled transfer*');
+        embedMsg.deletable && embedMsg.delete();
+        return;
+    }
+    const transferAmount = roundNumberTwoDecimals(Number(responseAmt));
+    const isValid = validateAmount(transferAmount, channel);
+    if (!isValid) {
+        embedMsg.deletable && embedMsg.delete();
+        return;
+    }
+    transferEmbed = BankVisualizer.getIOUTransferEmbed(sender, receiver, transferAmount);
+    await transferEmbed.edit(embedMsg);
+    channel.send('type a short reason/comment for the IOU: ');
+    const comment = (await getUserResponse(channel, sender.userId))?.content || '';
+    transferEmbed = BankVisualizer.getIOUTransferEmbed(sender, receiver, transferAmount, comment);
+    await transferEmbed.edit(embedMsg);
+    await BankVisualizer.getPreTransferConfirmationEmbed().send(channel);
+    const responseConfirmation = (await getUserResponse(channel, sender.userId))?.content;
+    if (responseConfirmation && responseConfirmation.toLowerCase() === 'yes') {
+        const transferResponse = bank.transferIOU(sender, receiver, transferAmount, comment);
+        if (transferResponse.success) {
+            await localStorage.saveData(bank.serializeData());
+            await receiver.getDiscordUser().send({
+                embeds: [
+                    BankVisualizer.getIOUTransferNotificationEmbed(
+                        sender.name,
+                        receiver,
+                        transferAmount,
+                        comment
+                    ).build(),
+                ],
+            });
+            await Logger.transactionLog(
+                `[IOU transfer] ${transferAmount} from ${sender.name} to ${receiver.name}\n` +
+                    `comment: ${comment || 'N/A'}`
+            );
+            await BankVisualizer.getIOUTransferReceiptEmbed(receiver.name, transferAmount).send(channel);
+        } else {
+            await BankVisualizer.getErrorEmbed(
+                `transfer failed: ${transferResponse.failReason || 'unknown reason'}`
+            ).send(channel);
+        }
+    } else {
+        channel.send('*cancelled transfer*');
+    }
+}
