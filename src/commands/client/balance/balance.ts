e0c0af5a6379bc45246d82e628b6c277e6da7c9f
Zain <reply2zain@gmail.com>
2023-02-27 18:47:14 -0500

    refactor: prevent concurrent transfer requests

--- src/commands/client/balance/balance.ts
+++ src/commands/client/balance/balance.ts
@@ -5,6 +5,7 @@ import { attachReactionToMessage } from '../../../utils/utils';
 import reactions from '../../../utils/reactions';
 import { commandHandler } from '../../../handlers/CommandHandler';
 import { MessageReaction } from 'discord.js';
+import { v4 as uuidv4 } from 'uuid';
 
 exports.run = async (event: MessageEventLocal) => {
     const balanceMsg = await BankVisualizer.showBalance(
@@ -20,18 +21,35 @@ exports.run = async (event: MessageEventLocal) => {
         let transferMsg = event.data.get('INITIAL_TRANSFER_MSG');
         if (transferMsg) {
             transferMsg.delete();
+            event.data.delete('REACTION_TSFR_REQ');
             event.data.delete('INITIAL_TRANSFER_MSG');
         }
+        let transferReq;
+        let cmdName;
         switch (reaction.emoji.name) {
             case reactions.MONEY:
-                await commandHandler.execute({ ...event, statement: 'transfer', args: [] });
+                cmdName = 'transfer';
                 break;
             case reactions.TICKET:
-                await commandHandler.execute({ ...event, statement: 'transferiou', args: [] });
+                cmdName = 'transferiou';
                 break;
             case reactions.PAGE_C:
-                await commandHandler.execute({ ...event, statement: 'iou', args: [] });
+                cmdName = 'iou';
                 break;
+            default:
+                return;
+        }
+        // allow the 'view iou' command to be called while a transfer is being initiated
+        if (cmdName.includes('transfer')) {
+            if (event.data.get('REACTION_TSFR_REQ')) return;
+            else {
+                transferReq = uuidv4();
+                event.data.set('REACTION_TSFR_REQ', transferReq);
+            }
+        }
+        await commandHandler.execute({ ...event, statement: cmdName, args: [] });
+        if (event.data.get('REACTION_TSFR_REQ') === transferReq) {
+            event.data.delete('REACTION_TSFR_REQ');
         }
     };
     await attachReactionToMessage(
