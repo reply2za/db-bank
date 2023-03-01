a89fdc5e7bc2c89972412c7be02fef24b0abd224
Zain <reply2zain@gmail.com>
2023-03-01 17:14:18 -0500

    docs: update data field descriptions

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -19,6 +19,12 @@ export type MessageEventLocal = {
 /*
 Data fields
 [name, type] - explanation
-INITIAL_TRANSFER_MSG, Message - the initial message object from the bot when starting a transfer
-REACTION_TSFR_REQ, string - contains an id for a reaction transfer request only when it is in progress
+[INITIAL_TRANSFER_MSG, Message] - active message object from the bot when starting a transfer
+- is created when a transfer message/request from the bot is sent
+- is deleted when a response is provided or timeouts
+[REACTION_TSFR_REQ, {id: string, cmdName: string}] - reaction transfer request
+- contains an id and command-name for a reaction transfer request only when it is in progress
+- is created only when a transfer request is initiated from a reaction
+- is deleted when the request is complete
+
  */
