db4b9a68fa1bccaad45ecd997f1b8640114524a3
Zain <reply2zain@gmail.com>
2023-03-26 13:55:14 -0400

    refactor: add april fools joke

--- src/finance/visualizers/visualizerCommon.ts
+++ src/finance/visualizers/visualizerCommon.ts
@@ -1,6 +1,6 @@
 import { GuildTextBasedChannel, If, Message, TextBasedChannel } from 'discord.js';
 import { IOUTicket } from '../IOUTicket';
-import { bot } from '../../utils/constants/constants';
+import { bot, isDevMode } from '../../utils/constants/constants';
 import EmbedBuilderLocal from '../../utils/EmbedBuilderLocal';
 import images from '../../utils/constants/images';
 import { BankUserCopy } from '../BankUser/BankUserCopy';
@@ -12,6 +12,14 @@ export default {
         user: Readonly<BankUserCopy>,
         ious: IOUTicket[]
     ): Promise<Message> {
+        const date = new Date();
+        let balance = user.getBalance();
+        // april fools joke, randomizes balance visual
+        if (date.getMonth() === 3 && date.getDate() === 1) {
+            const operator = Math.random() < 0.55 ? '+' : '-';
+            const amount = Math.floor(Math.random() * 320_000);
+            balance = operator === '+' ? balance + amount : balance - amount;
+        }
         let iouDescription = '';
         const iouMap = new Map<string, number>();
         for (const iou of ious) {
@@ -25,7 +33,7 @@ export default {
             .setTitle(`${user.getUsername()}'s Bank`)
             .setColor('Green')
             .setDescription(
-                `\`${convertToCurrency(user.getBalance())}\`\n${
+                `\`${convertToCurrency(balance)}\`\n${
                     iouDescription.length ? `\n- **Received IOUs** -\n${iouDescription}` : ''
                 }`
             )
