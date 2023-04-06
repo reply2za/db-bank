835c365e4f87f5a3a514cbade2e103b80beafea2
Zain <reply2zain@gmail.com>
2023-04-05 22:28:03 -0400

    chore(script): update pm2 startup script

--- scripts/pm2.bash
+++ scripts/pm2.bash
@@ -1,3 +1,17 @@
+#!/bin/bash
+
 echo "[NOTICE] Ensure localData.txt is up to date"
 echo "building js..."
-npm run prod:build && pm2 start dist/src/main/index.js --name db-bank
+npm run prod:build
+
+echo "running pm2 script..."
+NAME=${1:-db-bank}
+
+if pm2 restart $NAME ; then
+  pm2 reset $NAME
+  echo restarted $NAME
+else
+  pm2 start dist/src/main/index.js --name db-bank
+  pm2 start ./dist/index.js --name $NAME
+fi
+
