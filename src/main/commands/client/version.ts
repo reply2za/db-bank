c4d01b06b1c4735f24ab0bcf86bf2584721f079c
Zain <reply2zain@gmail.com>
2023-12-10 16:14:17 -0500

    refactor: add github link to version cmd

--- src/main/commands/client/version.ts
+++ src/main/commands/client/version.ts
@@ -1,7 +1,9 @@
-const { version } = require('../../../../package.json');
+import { version } from '../../../../package.json';
 import { EmbedBuilderLocal } from '@hoursofza/djs-common';
 import { MessageEventLocal } from '../../utils/types';
 
 exports.run = async (event: MessageEventLocal) => {
-    await new EmbedBuilderLocal().setDescription(version).send(event.message.channel);
+    await new EmbedBuilderLocal()
+        .setDescription(`[${version}](https://github.com/reply2za/db-bank)`)
+        .send(event.message.channel);
 };
